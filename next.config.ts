99eb76c3d3953b9277b1e8bed67c44267233b91a
Ynsoyn <ynsoyn@gmail.com>
2026-06-23 22:34:12 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/ynsoyn",
+  assetPrefix: "/ynsoyn",
 };
 
 export default nextConfig;
